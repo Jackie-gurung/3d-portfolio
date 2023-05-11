108acf226f644afc685f1227dc58d84dc3d73242
Jackie-Gurung <jamyang463gelek@gmail.com>
2023-05-11 08:57:08 +0545

    contact added

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -21,10 +21,7 @@ const Contact = () => {
 
   return (
   <div className={`xl:mt-12 flex xl:flex-row flex-col-reverse gap-10 overflow-hidden`}>
-  <motion.div
-    variants={slideIn("left", "tween", 0.2, 1)}
-    className='flex-[0.75] bg-black-100 p-8 rounded-2xl'
-  >
+  <motion.div variants={slideIn("left", "tween", 0.2, 1)} className='flex-[0.75] bg-black-100 p-8 rounded-2xl'>
     <p className={styles.sectionSubText}>Get in touch</p>
     <h3 className={styles.sectionHeadText}>Contact.</h3>
 
@@ -80,10 +77,10 @@ const Contact = () => {
     variants={slideIn("right", "tween", 0.2, 1)}
     className='xl:flex-1 xl:h-auto md:h-[550px] h-[350px]'
   >
-    <EarthCanvas />
+    <EarthCanvas/>
   </motion.div>
 </div>
 );
 }
 
-export default Contact
+export default SectionWrapper(Contact,"");
